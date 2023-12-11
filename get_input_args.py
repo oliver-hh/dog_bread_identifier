7bb7d9a0acb52bd2082f1519f2f4aa3756b5eb23
Oliver Brandt <oliver.brandt@m365-services.com>
2023-12-11 16:42:38 +0100

    Add author, beautify code

--- get_input_args.py
+++ get_input_args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
-# REVISED DATE: 
-# PURPOSE: Create a function that retrieves the following 3 command line inputs 
-#          from the user using the Argparse Python module. If the user fails to 
+#
+# PROGRAMMER:   Oliver Brandt
+# DATE CREATED: 10.12.2024
+# REVISED DATE:
+# PURPOSE: Create a function that retrieves the following 3 command line inputs
+#          from the user using the Argparse Python module. If the user fails to
 #          provide some or all of the 3 inputs, then the default values are
 #          used for the missing inputs. Command Line Arguments:
 #     1. Image Folder as --dir with default value 'pet_images'
@@ -17,10 +17,9 @@
 # Imports python modules
 import argparse
 
-# TODO 1: Define get_input_args function below please be certain to replace None
-#       in the return statement with parser.parse_args() parsed argument 
-#       collection that you created with this function
-# 
+# Define get_input_args function below please be certain to replace None
+# in the return statement with parser.parse_args() parsed argument 
+# collection that you created with this function
 def get_input_args():
     """
     Retrieves and parses the 3 command line arguments provided by the user when
